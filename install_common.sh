dfbbd8e42118a234e476a20f5edfdd6746895321
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-07-08 14:37:39 +0700

    Add script for common installation

---
+++ install_common.sh
@@ -0,0 +1,21 @@
+# htop, tmux, nano ...
+apt-get install nano htop tmux cmake libncurses5-dev libncursesw5-dev git
+
+# nvtop
+git clone https://github.com/Syllo/nvtop.git
+mkdir -p nvtop/build && cd nvtop/build
+cmake ..
+
+# If it errors with "Could NOT find NVML (missing: NVML_INCLUDE_DIRS)"
+# try the following command instead, otherwise skip to the build with make.
+cmake .. -DNVML_RETRIEVE_HEADER_ONLINE=True
+
+make
+make install # You may need sufficient permission for that (root)
+
+# drive-cli
+pip install drive-cli
+drive --remote login #--noauth_local_webserver
+
+#
+pip install tensorboardX --upgrade
