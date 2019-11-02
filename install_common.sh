8bacc9bdde0dcfaa397958753c0a45e3b384b309
Hoàng Tùng Lâm (Linus) <lamhoangtung.vz@gmail.com>
2019-11-02 22:14:22 +0700

    Update install_common.sh

--- install_common.sh
+++ install_common.sh
@@ -5,7 +5,7 @@ mv .bashrc.1 .bashrc
 source .bashrc
 
 # htop, tmux, nano ...
-apt-get -y install nano htop tmux cmake libncurses5-dev libncursesw5-dev git tree zip
+apt-get -y install nano htop tmux cmake libncurses5-dev libncursesw5-dev git tree zip expect
 
 # drive-cli
 cd ~
@@ -18,6 +18,9 @@ gdrive about
 # Upgrade tensorboardX to avoid some small bugs
 pip install tensorboardX --upgrade
 pip install ipdb trains
+pip install --upgrade imgaug
+pip install scikit-learn==0.20.3
+pip install tensorflow-gpu==1.14.0
 
 # nvtop
 conda deactivate
@@ -34,6 +37,9 @@ make install # You may need sufficient permission for that (root)
 # rclone for data backup
 curl https://rclone.org/install.sh | bash
 rclone config
+
+conda install -c eumetsat expect
+trains-init
 
 # Setting up swap
 # dd if=/dev/zero of=/swapfile-additional bs=1M count=32768
