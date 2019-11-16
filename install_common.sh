5627b53cca244af0550e2d886fe8fd471ffcfcec
Hoàng Tùng Lâm (Linus) <lamhoangtung.vz@gmail.com>
2019-11-16 15:40:29 +0700

    Update install_common.sh

--- install_common.sh
+++ install_common.sh
@@ -7,20 +7,10 @@ source .bashrc
 # htop, tmux, nano ...
 apt-get -y install nano htop tmux cmake libncurses5-dev libncursesw5-dev git tree zip expect
 
-# drive-cli
-cd ~
-wget https://github.com/gdrive-org/gdrive/releases/download/2.1.0/gdrive-linux-x64
-mv gdrive-linux-x64 gdrive
-chmod +x gdrive
-install gdrive /usr/local/bin/gdrive
-gdrive about
-
 # Upgrade tensorboardX to avoid some small bugs
-pip install tensorboardX --upgrade
-pip install ipdb trains
-pip install --upgrade imgaug
-pip install scikit-learn==0.20.3
-pip install tensorflow-gpu==1.14.0
+pip install ipdb trains scikit-learn==0.20.3 tensorflow-gpu==1.14.0
+pip install --upgrade imgaug tensorboardX
+conda install -c eumetsat expect
 
 # nvtop
 conda deactivate
@@ -34,13 +24,19 @@ cmake .. -DNVML_RETRIEVE_HEADER_ONLINE=True
 make
 make install # You may need sufficient permission for that (root)
 
+# drive-cli
+cd ~
+wget https://github.com/gdrive-org/gdrive/releases/download/2.1.0/gdrive-linux-x64
+mv gdrive-linux-x64 gdrive
+chmod +x gdrive
+install gdrive /usr/local/bin/gdrive
+gdrive about
+
 # rclone for data backup
 curl https://rclone.org/install.sh | bash
 rclone config
 
-conda install -c eumetsat expect
 trains-init
-
 # Setting up swap
 # dd if=/dev/zero of=/swapfile-additional bs=1M count=32768
 # mkswap /swapfile-additional
